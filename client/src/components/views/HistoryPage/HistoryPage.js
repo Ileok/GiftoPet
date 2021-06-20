f8c87330d572695eccc99a1f4f657c154d0f03d7
Ileok <apr_3@naver.com>
2021-06-21 02:07:15 +0900

    front update

---
+++ client/src/components/views/HistoryPage/HistoryPage.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function HistoryPage(props) {
+
+    return (
+        <div style={{ width: '80%', margin: '3rem auto' }}>
+            <div style={{ textAlign: 'center' }}>
+                <h1>주문 기록</h1>
+            </div>
+            <br />
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>주문번호</th>
+                        <th>가격</th>
+                        <th>수량</th>
+                        <th>구입일</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    {props.user.userData && props.user.userData.history &&
+                        props.user.userData.history.map(item => (
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.price}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.dateOfPurchase}</td>
+                            </tr>
+                        ))}
+
+
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default HistoryPage
---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,118 @@
+import React, { useState } from 'react'
+import { Typography, Button, Form, Input } from 'antd';
+import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
+const { TextArea } = Input;
+
+const Categories = [
+    { key: 1, value: "사료" },
+    { key: 2, value: "간식" },
+    { key: 3, value: "배변용품" },
+    { key: 4, value: "위생용품" },
+    { key: 5, value: "실내용품" },
+    { key: 6, value: "산책용품" },
+    { key: 7, value: "장난감" }
+]
+
+function UploadProductPage(props) {
+
+    const [Title, setTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Price, setPrice] = useState(0)
+    const [Category, setCategory] = useState(1)
+    const [Images, setImages] = useState([])
+
+    const titleChangeHandler = (event) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const descriptionChangeHandler = (event) => {
+        setDescription(event.currentTarget.value)
+    }
+
+    const priceChangeHandler = (event) => {
+        setPrice(event.currentTarget.value)
+    }
+
+    const categoryChangeHandler = (event) => {
+        setCategory(event.currentTarget.value)
+    }
+
+    const updateImages = (newImages) => {
+        setImages(newImages)
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        if (!Title || !Description || !Price || !Category || Images.length === 0) {
+            return alert(" 모든 값을 넣어주셔야 합니다.")
+        }
+
+
+        //서버에 채운 값들을 request로 보낸다.
+
+        const body = {
+            //로그인 된 사람의 ID 
+            writer: props.user.userData._id,
+            title: Title,
+            description: Description,
+            price: Price,
+            images: Images,
+            categories: Category
+        }
+
+        Axios.post('/api/product', body)
+            .then(response => {
+                if (response.data.success) {
+                    alert('상품 업로드에 성공 했습니다.')
+                    props.history.push('/')
+                } else {
+                    alert('상품 업로드에 실패 했습니다.')
+                }
+            })
+    }
+
+
+    return (
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <h2> 상품 업로드</h2>
+            </div>
+
+            <Form onSubmit={submitHandler}>
+                {/* DropZone */}
+                <FileUpload refreshFunction={updateImages} />
+
+                <br />
+                <br />
+                <label>이름</label>
+                <Input onChange={titleChangeHandler} value={Title} />
+                <br />
+                <br />
+                <label>설명</label>
+                <TextArea onChange={descriptionChangeHandler} value={Description} />
+                <br />
+                <br />
+                <label>가격</label>
+                <Input type="number" onChange={priceChangeHandler} value={Price} />
+                <br />
+                <br />
+                <select onChange={categoryChangeHandler} value={Category}>
+                    {Categories.map(item => (
+                        <option key={item.key} value={item.key}> {item.value}</option>
+                    ))}
+                </select>
+                <br />
+                <br />
+                <button type="submit">
+                    확인
+                </button>
+            </Form>
+
+
+        </div>
+    )
+}
+
+export default UploadProductPage
