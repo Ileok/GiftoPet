cae0ef022774fa0fd2784ddb3e15b9527909bd3f
umseozz <eum4494@naver.com>
2021-06-22 13:54:12 +0900

    Add Alert_Logout

--- server/routes/users.js
+++ server/routes/users.js
@@ -66,7 +66,10 @@ router.post("/login", (req, res) => {
 
 router.get("/logout", auth, (req, res) => {
     User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
-        if (err) return res.json({ success: false, err });
+        if (err){
+            alert('로그아웃 되었습니다.');
+            return res.json({ success: false, err });
+        }
         return res.status(200).send({
             success: true
         });
